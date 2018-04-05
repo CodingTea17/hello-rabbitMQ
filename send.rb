5d6a33fac7c7cd5acabd6470742a62f1cc663ba9
Dawson M <dawson.mortenson@gmail.com>
2018-04-05 10:36:05 -0700

    add a send/producer client script

--- send.rb
+++ send.rb
@@ -1,1 +1,22 @@
 require 'bunny'
+
+# Define a new connection to the RabbitMQ Server
+# Bunny.new(hostname: 'rabbit.local')
+connection = Bunny.new
+connection.start
+
+# Create a channel to utilize API calls
+channel = conneciton.create_channel
+
+# Create queue
+queue = channel.queue('hello')
+
+# Define a message to send
+message = 'Hello Rabbit!'
+
+# Send the message
+channel.default_exchange.publish(message, routing_key: queue.name)
+puts " [x] Sent '#{message}'"
+
+# Terminate the connection
+connection.close
